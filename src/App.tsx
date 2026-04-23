08c87c23d13ce9011ac964aebacfa1ad1fce7601
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-23 18:42:25 -0400

    setting up axios api functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ProductProvider } from "./context/ProductContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   
+  const client = new QueryClient()
 
   return (
-    <ProductProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/profile' element = {<Profile />} />
-      </Routes>
-    </BrowserRouter>
-    </ProductProvider>
+    <QueryClientProvider client={client}>
+      <ProductProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element = {<Home />} />
+            <Route path='/profile' element = {<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -10,3 +10,5 @@ export interface Product {
             count: number,
         };
     }
+
+export type Category = string;
