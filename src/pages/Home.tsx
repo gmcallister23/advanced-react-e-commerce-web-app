eb65c6d38e9252b3406c8f9118148ef02e568226
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-22 21:03:35 -0400

    updating to use global state instead of local state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,25 @@
 import { useEffect, useState} from 'react';
 import type { Product } from '../types/types';
 import ProductCard  from '../components/ProductCard';
+import { useProductContext } from '../context/ProductContext';
 
 const Home: React.FC = () => {
 
     
 
-    const [products, setProducts] = useState<Product[]>([]);
-
+    //const [products, setProducts] = useState<Product[]>([]);
+    const {products, dispatch, selectedCategory} = useProductContext()
     
 
     useEffect(() => {
         const fetchProducts = async() => {
             const response = await fetch("https://fakestoreapi.com/products");
             const data = await response.json();
-            setProducts(data);
+            //setProducts(data);
+            dispatch({type:'SET_PRODUCTS', payload: data})//--> same as setProducts, just for global state
         };
         fetchProducts();
-    }, []);
+    }, [dispatch]);
 
     return (
      <div className="d-flex flex-wrap p-2 justify-content-center">
