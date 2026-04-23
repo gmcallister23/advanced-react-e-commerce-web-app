199c44d420c3d83512204dee3d390e4ec607b61c
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-23 19:01:41 -0400

    applied useQuery and added drop down category menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState} from 'react';
-import type { Product } from '../types/types';
+import type { Product, Category } from '../types/types';
 import ProductCard  from '../components/ProductCard';
 import { useProductContext } from '../context/ProductContext';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { fetchProducts } from '../api/api';
+import { fetchCategories } from '../api/api';
 
 const Home: React.FC = () => {
 
@@ -11,23 +14,50 @@ const Home: React.FC = () => {
     //const [products, setProducts] = useState<Product[]>([]);
     const {products, dispatch, selectedCategory} = useProductContext()
     
+    const { data: productsData, isLoading }  = useQuery({
+        queryKey: ['products'],
+        queryFn: fetchProducts,
+
+    });
 
     useEffect(() => {
-        const fetchProducts = async() => {
-            const response = await fetch("https://fakestoreapi.com/products");
-            const data = await response.json();
-            //setProducts(data);
-            dispatch({type:'SET_PRODUCTS', payload: data})//--> same as setProducts, just for global state
-        };
-        fetchProducts();
-    }, [dispatch]);
+        if (productsData)
+        dispatch({ type: 'SET_PRODUCTS', payload: productsData.data })
+    }, [dispatch, productsData]);
+
+    const { data: categories } = useQuery({
+        queryKey: ['categories'],
+        queryFn: fetchCategories,
+    }); 
+
+
+    // useEffect(() => {
+    //     const fetchProducts = async() => {
+    //         const response = await fetch("https://fakestoreapi.com/products");
+    //         const data = await response.json();
+    //         //setProducts(data);
+    //         dispatch({type:'SET_PRODUCTS', payload: data})//--> same as setProducts, just for global state
+    //     };
+    //     fetchProducts();
+    // }, [dispatch]);
 
     return (
         <div>
+            <select>
+                <option value=''> All Categories</option>
+                {categories?.data.map((category:Category) => (
+                    <option value={category} key={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
+            
             <button onClick={() => navigate('/profile')}>Go to Profile Page</button>
-     <div className="d-flex flex-wrap p-2 justify-content-center">
+           {isLoading && (<h1>Loading...</h1>)}
+            
+            <div className="d-flex flex-wrap p-2 justify-content-center">
         {products.map((product: Product) => (
-            <ProductCard product={product} />
+            <ProductCard product={product} key={product.id} />
         ))}
      </div>
      </div>
