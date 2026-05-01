d47cba965be605b5bd9aba34af6a3c8f7db1217e
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-04-30 20:27:13 -0400

    using React Routes to connect navbar to the rest of the app, updating total quantity method so that it works in the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,19 @@
 import { useSelector } from 'react-redux';
 import type { RootState } from '../store/store';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    const totalQuantity = useSelector((state: RootState) => state.cart.totalQuantity);
-
+    const items = useSelector((state: RootState) => state.cart.items);
+    
+    const totalQuantity = items.reduce(
+        (sum, item) => sum + item.quantity, 0
+    );
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href='/'>Shop</a>
+            <Link className="navbar-brand" to='/'>Shop</Link>
                 <div className="navbar-nav">
-                    <a className="nav-item nav-link" href="/profile">Profile</a>
-                    <a className="nav-item nav-link" href="/cart">Cart({totalQuantity})</a>
+                    <Link className="nav-item nav-link" to="/profile">Profile</Link>
+                    <Link className="nav-item nav-link" to="/cart">Cart({totalQuantity})</Link>
                 </div>
         </nav>
 
