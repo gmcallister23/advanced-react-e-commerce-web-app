450beb290f7cb7d48b8ad9401730f661a2d43fb1
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-05-14 20:17:15 -0400

    set up login, logout, register pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { ProductProvider } from "./context/ProductContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Cart from "./pages/Cart";
 import { AuthProvider } from "./context/AuthContext";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 
 const client = new QueryClient()
 
@@ -20,6 +23,10 @@ function App() {
               <Route path='/' element = {<Home />} />
               <Route path='/profile' element = {<Profile />} />
               <Route path='/cart' element = {<Cart />} />
+              <Route path='/register' element = {<Register />} />
+              <Route path='/login' element = {<Login />} />
+              <Route path='/cart' element = {<Logout />} />
+
             </Routes>
           </BrowserRouter>
         </AuthProvider>
