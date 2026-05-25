04e8a02ce71d87ab42fef87e970b7d1eb8985a10
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-05-25 16:36:16 -0400

    building unit test for cartItem component

--- src/__tests__/Cart.test.tsx
+++ src/__tests__/Cart.test.tsx
@@ -1,12 +1,27 @@
 //Unit Test
 
 import { render, screen } from '@testing-library/react';
-import Cart from '../pages/Cart';
+import CartItem from '../components/CartItem';
 
-describe('Cart', () => {
-    it('shows empty cart message', () => {
-        render(<Cart items={()} />);
+describe('CartItem', () => {
+    it('renders cart item info', () => {
+        
+        const item = {
+            productId: '1',
+            title: 'Baseball Cleats',
+            price: 75,
+            quantity: 1,
+            description: '',
+            image: '',
+        }
+
+        
+        render(<CartItem item={item} />);
 
         expect(screen.getByText(/cart is empty/i)).toBeInTheDocument();
     });
-})
+
+        expect(screen.getByText('Baseball Cleats')).toBeInTheDocument();
+        expect(screen.getByText('Quantity: 1')).toBeInTheDocument();
+        expect(screen.getByText('$75')).toBeInTheDocument();
+    })
